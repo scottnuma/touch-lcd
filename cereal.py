95b3e44f7890806b7e27d3aa39f61b32e3d0c1bb
Scott Numamoto <scott.numamoto@gmail.com>
2016-08-15 12:28:33 -0700

    Improve readability

--- cereal.py
+++ cereal.py
@@ -3,23 +3,47 @@
 import serial, time, io
 
 class Display():
-    def __init__(self, port = "/dev/tty.usbmodem1412"):
+    """Controls a ezLCD-403 connected via USB"""
+    def __init__(self, port = "/dev/tty.usbmodem1412", baud = 115200):
         self.port = port
-        self.baud = 115200
+        self.baud = baud
         self.ser = serial.Serial(self.port, self.baud, timeout=.05)
         self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser), newline=None)
         assert self.ser.isOpen(), "%s is not open" % self.port
-    
+
+        self.cls()
+
+        # Variables that are stored in the display
+        self.font = "sans"
+        self.font_size = 14
+        self.orientation = 0
+        self.x = 0
+        self.y = 0
+        self.xy(self.x, self.y)
+
+    def cursor_location(self):
+        """Returns a tuple containing the (x,y) of the cursor location
+
+        >>> a = Display()
+        >>> x = 43
+        >>> y = 98
+        >>> a.xy(x, y)
+        >>> a.cursor_location()
+        Code: 43 98 - xy
+        (43, 98)
+        """
+        pos_str = self.cmd("xy")
+        coor = pos_str.split()
+        result = (int(coor[0]), int(coor[1]))
+        assert (self.x, self.y) == result, "Location variables not working"
+        return result
+
     def xy(self, x=None, y=None):
         """Moves the cursor to specificed x and y
 
         x = 50 and y = 100 will place the cursor 50 pixels
         from the right and 100 pixels down
         """
-        if x == None and y == None:
-            pos_str = self.cmd("xy")
-            coor = pos_str.split()
-            return (int(coor[0]), int(coor[1]))
 
         if x != None and y == None:
             lat = ["L","C","R","l","c","r"]
@@ -28,46 +52,57 @@ class Display():
             assert x[0] in lat, "Not a valid position"
             assert x[1] in vert, "Not a valid position"
 
-            return self.cmd("xy " + x)
-            
-        self.cmd("xy %s %s" % (x, y))
+            self.cmd("xy " + x)
+
+        if x != self.x or y != self.y:
+            self.x = x
+            self.y = y
+            self.cmd("xy %s %s" % (x, y))
 
     def line(self, x1, y1, x2, y2, line_type="solid", line_width="thin"):
-        """Draws a line from (x1, y1) to (x2, y2)"""
+        """Draws a line from (x1, y1) to (x2, y2)
+        >>> a = Display()
+        >>> x1, y1, x2, y2 = 43, 98, 123, 60
+        >>> a.line(x1, y1, x2, y2, "dotted", "thick")
+        """
+        #Set the starting location of the line
         self.xy(x1, y1)
 
         # Set the line type
         line_types = ["solid", "dotted", "dashed"]
-        try:
-            type_index = line_types.index(line_type)
-        except ValueError as e:
-            raise ValueError("%s is not a valid line style" % line_type)
+        type_index = line_types.index(line_type)
         self.cmd("linetype %s" % type_index)
 
         # Set the line width
         line_widths = ["thin", "thick"]
-        try:
-            width_index = line_widths.index(line_width)
-        except ValueError as e:
-            raise ValueError("%s is not a valid line width" % line_width)
+        width_index = line_widths.index(line_width)
         self.cmd("linewidth %s" % width_index)
 
         self.cmd("line %s %s" % (x2, y2))
 
     def print(self, string, x=None, y=None, font="sans", size=14, orientation=0):
+        """Prints string to the screen
+        >>> a = Display()
+        >>> a.print("testing", 50, 50)
+        """
+
         # if x and y are not specified, they default to the previous point
-        if x and y:
+        if x != None or y != None:
             self.xy(x, y)
 
-        self.cmd("font %s%s" % (font, size))
+        if font != self.font or size != self.font_size:
+            self.font = font
+            self.font_size = size
+            self.cmd("font %s%s" % (font, size))
 
-        t_orientation = orientation
-        if t_orientation > 3:
-            t_orientation /= 90
-            t_orientation = int(t_orientation)
-        assert -1 < t_orientation < 4, "%s rotation is not valid" % orientation
-        self.cmd('fonto %s' % t_orientation)
+        if self.orientation != orientation:
+            self.cmd('fonto %s' % self.orientation)
+
         self.cmd('print "%s"' % string)
+
+    def new_line(self):
+        """Moves the cursor down one line"""
+        self.xy(self.x, self.y + self.font_size)
 
     def cmd(self, string):
         """Issues a command to the display
@@ -102,13 +137,9 @@ class Display():
         y = int(self.cmd("touchy").strip())
         return (x,y)
 
-    def process_touch(self):
-        """Returns an int indicating touch status"""
-        return int(self.cmd("touchs").strip())
-
     def is_touching(self):
         """Returns boolean on touch status"""
-        return self.process_touch() == 3
+        return int(self.cmd("touchs").strip()) == 3
 
     def wait_for_touch(self):
         """Return touch coordinate after waiting"""
@@ -122,20 +153,25 @@ class Display():
 
         return self.touch()
 
-    def line_draw(self):
+    def tap_line(self):
+        """Connects a line between two taps"""
         first = self.wait_for_touch()
         second = self.wait_for_touch()
         self.line(first[0],first[1],second[0],second[1])
 
     def live_tap(self):
+        """Prints the coordinate of tap at the location of tap
+
+        Useful for seeing tap interpretation"""
+
         p = self.wait_for_touch()
         self.print(str(p), p[0], p[1], font=2)
 
     def live(self):
-        order = "stuff"
-        while True and order:
+        """Enter live interface with display using display's language"""
+        while True:
             order = input("> ")
-            if order == 'exit':
+            if not order or order == 'exit':
                 self.ser.close()
                 break
             elif order == 'help':
@@ -143,17 +179,9 @@ class Display():
             else:
                 self.cmd(order)
 
-    def cls(self, color="white"):
+    def cls(self):
+        """Clears the screen to white with black text"""
         self.cmd("cls white black")
-
-    def color_cycle(self):
-        for r in reversed(range(0, 256, 10)):
-            for g in range(0, 256, 10):
-                for b in range(0, 256, 10):
-                    command ='colorid 180 %s %s %s' % (r,g,b)
-                    self.cmd(command)
-                    self.cmd('cls 180')
-                    time.sleep(.1)
 
 def remove_new_line(string):
     """Removes a new line at the end of a character if present"""
